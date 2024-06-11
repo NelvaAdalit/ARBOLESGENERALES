7ad01d46ff21ad26e9fb6505c720cbe4a53820cf
NelvaAdalit <moraadalid378@gmail.com>
2024-06-11 16:42:37 -0400

    Change

--- ARBOLESGENERALES/GestionColegio.h
+++ ARBOLESGENERALES/GestionColegio.h
@@ -31,7 +31,7 @@ namespace ARBOLESGENERALES {
         ArbolGeneral^ Arbol = gcnew ArbolGeneral();
     private : System::Windows::Forms::TextBox^ TextRaiz;
     private: System::Windows::Forms::TextBox^ TextinsertR;
-    private: System::Windows::Forms::Button^ InsertRaiz;
+
     private: System::Windows::Forms::TextBox^ TextPadre;
     private: System::Windows::Forms::TextBox^ TextHijo;
     private: System::Windows::Forms::Button^ InsertHijo;
@@ -40,7 +40,7 @@ namespace ARBOLESGENERALES {
     private: System::Windows::Forms::Button^ Entre_Orden;
     private: System::Windows::Forms::Button^ Post_Orden;
     private: System::Windows::Forms::TextBox^ TextOrden;
-    private: System::Windows::Forms::Label^ label1;
+
     private: System::Windows::Forms::Label^ label2;
     private: System::Windows::Forms::Label^ label3;
     private: System::Windows::Forms::Label^ labelRol;
@@ -53,6 +53,7 @@ namespace ARBOLESGENERALES {
     private: System::Windows::Forms::TextBox^ TextDesc;
     private: System::Windows::Forms::Label^ labelResp;
     private: System::Windows::Forms::TextBox^ TextResp;
+    private: System::Windows::Forms::Label^ label1;
 
            System::ComponentModel::Container^ components;
 
@@ -60,7 +61,6 @@ namespace ARBOLESGENERALES {
            void InitializeComponent(void)
            {
                this->TextinsertR = (gcnew System::Windows::Forms::TextBox());
-               this->InsertRaiz = (gcnew System::Windows::Forms::Button());
                this->TextPadre = (gcnew System::Windows::Forms::TextBox());
                this->TextHijo = (gcnew System::Windows::Forms::TextBox());
                this->InsertHijo = (gcnew System::Windows::Forms::Button());
@@ -69,7 +69,6 @@ namespace ARBOLESGENERALES {
                this->Entre_Orden = (gcnew System::Windows::Forms::Button());
                this->Post_Orden = (gcnew System::Windows::Forms::Button());
                this->TextOrden = (gcnew System::Windows::Forms::TextBox());
-               this->label1 = (gcnew System::Windows::Forms::Label());
                this->label2 = (gcnew System::Windows::Forms::Label());
                this->label3 = (gcnew System::Windows::Forms::Label());
                this->labelRol = (gcnew System::Windows::Forms::Label());
@@ -82,6 +81,7 @@ namespace ARBOLESGENERALES {
                this->TextDesc = (gcnew System::Windows::Forms::TextBox());
                this->labelResp = (gcnew System::Windows::Forms::Label());
                this->TextResp = (gcnew System::Windows::Forms::TextBox());
+               this->label1 = (gcnew System::Windows::Forms::Label());
                this->SuspendLayout();
                // 
                // TextinsertR
@@ -91,33 +91,26 @@ namespace ARBOLESGENERALES {
                this->TextinsertR->Size = System::Drawing::Size(120, 20);
                this->TextinsertR->TabIndex = 0;
                // 
-               // InsertRaiz
-               // 
-               this->InsertRaiz->Location = System::Drawing::Point(447, 73);
-               this->InsertRaiz->Name = L"InsertRaiz";
-               this->InsertRaiz->Size = System::Drawing::Size(118, 24);
-               this->InsertRaiz->TabIndex = 1;
-               this->InsertRaiz->Text = L"Insert Raíz";
-               this->InsertRaiz->UseVisualStyleBackColor = true;
-               this->InsertRaiz->Click += gcnew System::EventHandler(this, &GestionColegio::InsertRaiz_Click);
-               // 
                // TextPadre
                // 
-               this->TextPadre->Location = System::Drawing::Point(368, 146);
+               this->TextPadre->Location = System::Drawing::Point(154, 91);
                this->TextPadre->Name = L"TextPadre";
                this->TextPadre->Size = System::Drawing::Size(100, 20);
                this->TextPadre->TabIndex = 2;
                // 
                // TextHijo
                // 
-               this->TextHijo->Location = System::Drawing::Point(474, 146);
+               this->TextHijo->Location = System::Drawing::Point(387, 93);
                this->TextHijo->Name = L"TextHijo";
                this->TextHijo->Size = System::Drawing::Size(100, 20);
                this->TextHijo->TabIndex = 3;
+               this->TextHijo->TextChanged += gcnew System::EventHandler(this, &GestionColegio::TextHijo_TextChanged);
                // 
                // InsertHijo
                // 
-               this->InsertHijo->Location = System::Drawing::Point(368, 172);
+               this->InsertHijo->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->InsertHijo->Location = System::Drawing::Point(12, 133);
                this->InsertHijo->Name = L"InsertHijo";
                this->InsertHijo->Size = System::Drawing::Size(206, 23);
                this->InsertHijo->TabIndex = 4;
@@ -127,7 +120,9 @@ namespace ARBOLESGENERALES {
                // 
                // EliminarHijo
                // 
-               this->EliminarHijo->Location = System::Drawing::Point(368, 201);
+               this->EliminarHijo->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->EliminarHijo->Location = System::Drawing::Point(239, 133);
                this->EliminarHijo->Name = L"EliminarHijo";
                this->EliminarHijo->Size = System::Drawing::Size(206, 23);
                this->EliminarHijo->TabIndex = 5;
@@ -137,7 +132,9 @@ namespace ARBOLESGENERALES {
                // 
                // Pre_Orden
                // 
-               this->Pre_Orden->Location = System::Drawing::Point(368, 267);
+               this->Pre_Orden->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->Pre_Orden->Location = System::Drawing::Point(12, 268);
                this->Pre_Orden->Name = L"Pre_Orden";
                this->Pre_Orden->Size = System::Drawing::Size(206, 23);
                this->Pre_Orden->TabIndex = 6;
@@ -147,7 +144,9 @@ namespace ARBOLESGENERALES {
                // 
                // Entre_Orden
                // 
-               this->Entre_Orden->Location = System::Drawing::Point(368, 296);
+               this->Entre_Orden->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->Entre_Orden->Location = System::Drawing::Point(262, 268);
                this->Entre_Orden->Name = L"Entre_Orden";
                this->Entre_Orden->Size = System::Drawing::Size(206, 23);
                this->Entre_Orden->TabIndex = 7;
@@ -157,7 +156,9 @@ namespace ARBOLESGENERALES {
                // 
                // Post_Orden
                // 
-               this->Post_Orden->Location = System::Drawing::Point(368, 325);
+               this->Post_Orden->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->Post_Orden->Location = System::Drawing::Point(539, 270);
                this->Post_Orden->Name = L"Post_Orden";
                this->Post_Orden->Size = System::Drawing::Size(206, 23);
                this->Post_Orden->TabIndex = 8;
@@ -167,51 +168,48 @@ namespace ARBOLESGENERALES {
                // 
                // TextOrden
                // 
-               this->TextOrden->Location = System::Drawing::Point(580, 270);
+               this->TextOrden->Location = System::Drawing::Point(192, 318);
                this->TextOrden->Multiline = true;
                this->TextOrden->Name = L"TextOrden";
-               this->TextOrden->Size = System::Drawing::Size(208, 78);
+               this->TextOrden->Size = System::Drawing::Size(372, 86);
                this->TextOrden->TabIndex = 9;
-               // 
-               // label1
-               // 
-               this->label1->AutoSize = true;
-               this->label1->Location = System::Drawing::Point(444, 25);
-               this->label1->Name = L"label1";
-               this->label1->Size = System::Drawing::Size(72, 13);
-               this->label1->TabIndex = 10;
-               this->label1->Text = L"Insertar Raíz:";
                // 
                // label2
                // 
                this->label2->AutoSize = true;
-               this->label2->Location = System::Drawing::Point(365, 130);
+               this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->label2->Location = System::Drawing::Point(12, 91);
                this->label2->Name = L"label2";
-               this->label2->Size = System::Drawing::Size(91, 13);
+               this->label2->Size = System::Drawing::Size(127, 20);
                this->label2->TabIndex = 11;
                this->label2->Text = L"Insertar a Padre:";
                // 
                // label3
                // 
                this->label3->AutoSize = true;
-               this->label3->Location = System::Drawing::Point(471, 130);
+               this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->label3->Location = System::Drawing::Point(281, 91);
                this->label3->Name = L"label3";
-               this->label3->Size = System::Drawing::Size(74, 13);
+               this->label3->Size = System::Drawing::Size(100, 20);
                this->label3->TabIndex = 12;
                this->label3->Text = L"Nombre Hijo:";
                // 
                // labelRol
                // 
                this->labelRol->AutoSize = true;
-               this->labelRol->Location = System::Drawing::Point(365, 227);
+               this->labelRol->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->labelRol->Location = System::Drawing::Point(12, 199);
                this->labelRol->Name = L"labelRol";
-               this->labelRol->Size = System::Drawing::Size(58, 13);
+               this->labelRol->Size = System::Drawing::Size(100, 20);
                this->labelRol->TabIndex = 13;
                this->labelRol->Text = L"Rol Persona:";
                // 
                // TextRol
                // 
-               this->TextRol->Location = System::Drawing::Point(429, 224);
+               this->TextRol->Location = System::Drawing::Point(109, 196);
                this->TextRol->Name = L"TextRol";
                this->TextRol->Size = System::Drawing::Size(145, 20);
                this->TextRol->TabIndex = 14;
@@ -219,15 +217,17 @@ namespace ARBOLESGENERALES {
                // labelEdad
                // 
                this->labelEdad->AutoSize = true;
-               this->labelEdad->Location = System::Drawing::Point(580, 227);
+               this->labelEdad->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->labelEdad->Location = System::Drawing::Point(290, 203);
                this->labelEdad->Name = L"labelEdad";
-               this->labelEdad->Size = System::Drawing::Size(32, 13);
+               this->labelEdad->Size = System::Drawing::Size(51, 20);
                this->labelEdad->TabIndex = 15;
                this->labelEdad->Text = L"Edad:";
                // 
                // TextEdad
                // 
-               this->TextEdad->Location = System::Drawing::Point(618, 224);
+               this->TextEdad->Location = System::Drawing::Point(349, 200);
                this->TextEdad->Name = L"TextEdad";
                this->TextEdad->Size = System::Drawing::Size(48, 20);
                this->TextEdad->TabIndex = 16;
@@ -235,15 +235,17 @@ namespace ARBOLESGENERALES {
                // labelGenero
                // 
                this->labelGenero->AutoSize = true;
-               this->labelGenero->Location = System::Drawing::Point(672, 227);
+               this->labelGenero->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->labelGenero->Location = System::Drawing::Point(483, 207);
                this->labelGenero->Name = L"labelGenero";
-               this->labelGenero->Size = System::Drawing::Size(45, 13);
+               this->labelGenero->Size = System::Drawing::Size(67, 20);
                this->labelGenero->TabIndex = 17;
-               this->labelGenero->Text = L"Género:";
+               this->labelGenero->Text = L"Genero:";
                // 
                // TextGenero
                // 
-               this->TextGenero->Location = System::Drawing::Point(723, 224);
+               this->TextGenero->Location = System::Drawing::Point(567, 207);
                this->TextGenero->Name = L"TextGenero";
                this->TextGenero->Size = System::Drawing::Size(65, 20);
                this->TextGenero->TabIndex = 18;
@@ -251,15 +253,17 @@ namespace ARBOLESGENERALES {
                // labelDesc
                // 
                this->labelDesc->AutoSize = true;
-               this->labelDesc->Location = System::Drawing::Point(580, 130);
+               this->labelDesc->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->labelDesc->Location = System::Drawing::Point(493, 93);
                this->labelDesc->Name = L"labelDesc";
-               this->labelDesc->Size = System::Drawing::Size(69, 13);
+               this->labelDesc->Size = System::Drawing::Size(96, 20);
                this->labelDesc->TabIndex = 19;
                this->labelDesc->Text = L"Descripción:";
                // 
                // TextDesc
                // 
-               this->TextDesc->Location = System::Drawing::Point(583, 146);
+               this->TextDesc->Location = System::Drawing::Point(595, 91);
                this->TextDesc->Name = L"TextDesc";
                this->TextDesc->Size = System::Drawing::Size(206, 20);
                this->TextDesc->TabIndex = 20;
@@ -267,25 +271,40 @@ namespace ARBOLESGENERALES {
                // labelResp
                // 
                this->labelResp->AutoSize = true;
-               this->labelResp->Location = System::Drawing::Point(580, 169);
+               this->labelResp->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->labelResp->Location = System::Drawing::Point(470, 138);
                this->labelResp->Name = L"labelResp";
-               this->labelResp->Size = System::Drawing::Size(70, 13);
+               this->labelResp->Size = System::Drawing::Size(107, 20);
                this->labelResp->TabIndex = 21;
                this->labelResp->Text = L"Responsable:";
                // 
                // TextResp
                // 
-               this->TextResp->Location = System::Drawing::Point(583, 185);
+               this->TextResp->Location = System::Drawing::Point(583, 140);
                this->TextResp->Name = L"TextResp";
                this->TextResp->Size = System::Drawing::Size(206, 20);
                this->TextResp->TabIndex = 22;
                // 
+               // label1
+               // 
+               this->label1->AutoSize = true;
+               this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+                   static_cast<System::Byte>(0)));
+               this->label1->Location = System::Drawing::Point(258, 20);
+               this->label1->Name = L"label1";
+               this->label1->Size = System::Drawing::Size(198, 24);
+               this->label1->TabIndex = 23;
+               this->label1->Text = L"GESTION COLEGIO";
+               this->label1->Click += gcnew System::EventHandler(this, &GestionColegio::label1_Click_1);
                // 
                // GestionColegio
                // 
                this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
                this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+               this->BackColor = System::Drawing::SystemColors::ActiveCaption;
                this->ClientSize = System::Drawing::Size(800, 450);
+               this->Controls->Add(this->label1);
                this->Controls->Add(this->TextResp);
                this->Controls->Add(this->labelResp);
                this->Controls->Add(this->TextDesc);
@@ -298,7 +317,6 @@ namespace ARBOLESGENERALES {
                this->Controls->Add(this->labelRol);
                this->Controls->Add(this->label3);
                this->Controls->Add(this->label2);
-               this->Controls->Add(this->label1);
                this->Controls->Add(this->TextOrden);
                this->Controls->Add(this->Post_Orden);
                this->Controls->Add(this->Entre_Orden);
@@ -307,12 +325,11 @@ namespace ARBOLESGENERALES {
                this->Controls->Add(this->InsertHijo);
                this->Controls->Add(this->TextHijo);
                this->Controls->Add(this->TextPadre);
-               this->Controls->Add(this->InsertRaiz);
-               this->Controls->Add(this->TextRaiz);
                this->Name = L"GestionColegio";
                this->Text = L"GestionColegio";
                this->ResumeLayout(false);
                this->PerformLayout();
+
            }
 #pragma endregion
     private: System::Void InsertRaiz_Click(System::Object^ sender, System::EventArgs^ e) {
@@ -333,7 +350,13 @@ namespace ARBOLESGENERALES {
     private: System::Void Post_Orden_Click(System::Object^ sender, System::EventArgs^ e) {
         // Logic for post-order traversal
     }
-    };
+    private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+    }
+private: System::Void label1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void TextHijo_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
 
 
